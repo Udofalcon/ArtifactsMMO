fce206c90568533b120cce96132135a056221561
Udofalcon <bear.arms.cavalry@gmail.com>
2026-07-06 19:01:26 -0400

    Added achievment fetch error handling.

--- endpoints/achievements.js
+++ endpoints/achievements.js
@@ -1,13 +1,26 @@
 const AchievementSchema = require('../schemas/achievements/achievement');
 
 class Achievements {
-    async GetAchievements(args) { // { page, size, type }
-        const url = `https://api.artifactsmmo.com/achievements`;
-        const headers = {
-            Accept: 'application/json',
-        };
-
+    async GetAchievements({
+        page = 1,
+        size = 50,
+        type
+    }) {
         try {
+            if (!this.isValidPage(page)) throw Error(`Page Error`);
+            if (!this.isValidSize(size)) throw Error(`Size Error`);
+            if (!this.isValidType(type)) throw Error(`Type Error`);
+
+            const args = [
+                `page=${page}`,
+                `size=${size}`,
+                type !== undefined ? `type=${type}` : ''
+            ];
+            const url = `https://api.artifactsmmo.com/achievements?${args.join('&')}`;
+            const headers = {
+                Accept: 'application/json',
+            };
+
             const response = await fetch(url, { method: 'GET', headers });
             const { data } = await response.json();
 
@@ -23,13 +36,17 @@ class Achievements {
         }
     }
 
-    async GetAchievement({ code }) {
-        const url = `https://api.artifactsmmo.com/achievements/${code}`;
-        const headers = {
-            Accept: 'application/json',
-        };
-
+    async GetAchievement({
+        code
+    }) {
         try {
+            if (!this.isValidCode(code)) throw Error(`Code Error`);
+
+            const url = `https://api.artifactsmmo.com/achievements/${code}`;
+            const headers = {
+                Accept: 'application/json',
+            };
+
             const response = await fetch(url, { method: 'GET', headers });
             const { data } = await response.json();
 
@@ -37,6 +54,36 @@ class Achievements {
         } catch (error) {
             console.error('Get Achievement', error);
         }
+    }
+
+    isValidPage(page) {
+        return page >= 1;
+    }
+
+    isValidSize(size) {
+        return size >= 1 && size <= 10000;
+    }
+
+    isValidType(type) {
+        const valid = [
+            'combat_kill',
+            'combat_drop',
+            'combat_level',
+            'gathering',
+            'crafting',
+            'recycling',
+            'task',
+            'other',
+            'use',
+            'npc_buy',
+            'npc_sell'
+        ];
+
+        return type === undefined || valid.includes(type);
+    }
+
+    isValidCode(code) {
+        return code.match(/^[a-zA-Z0-9_-]+$/);
     }
 }
 
